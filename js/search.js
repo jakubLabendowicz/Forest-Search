7f153fe4f6b772c978fe648cd6d342b8be24445d
jakubLabedowicz <jalabedowicz@gmail.com>
2021-10-19 15:04:57 +0200

    edit

--- js/search.js
+++ js/search.js
@@ -1,3 +1,81 @@
+function addZero(variable) {
+  if(variable<10) {
+    return "0" + variable;
+  }
+  else {
+    return variable;
+  }
+}
+
+function setHistory(data) {
+  var text = document.getElementById('searchText'+data).value;
+  var search = document.getElementById('searchSelector'+data).value;
+
+  const historyDate = new Date();
+  var hours = historyDate.getHours();
+  var minutes = historyDate.getMinutes();
+  var secounds = historyDate.getSeconds();
+  var days = historyDate.getDate();
+  var month = historyDate.getMonth() + 1;
+  var year = historyDate.getFullYear();
+
+  hours = addZero(hours);
+  minutes = addZero(minutes);
+  secounds = addZero(secounds);
+  days = addZero(days);
+  month = addZero(month);
+
+  var hour = hours + ":" + minutes + ":" + secounds;
+  var date = days + "." + month + "." + year;
+
+  var link = Search.getLink();
+
+  var historyElement = {
+    time: {
+      hour: hour,
+      date: date
+    },
+    text: text,
+    search: search,
+    link: link
+  }
+
+  if(localStorage.getItem("history") == null) {
+    localStorage.setItem("history", "");
+  }
+
+  if(localStorage.getItem("searchCounter") == null) {
+    localStorage.setItem("searchCounter", "");
+  }
+
+  if(text != "") {
+    var preHistory = localStorage.getItem("history");
+
+    if(preHistory == "") {
+      localStorage.setItem("history", "[" + JSON.stringify(historyElement) + "]");
+    }
+    else {
+      var cutPreHistory = preHistory.substring(0, preHistory.lastIndexOf("]"));
+      localStorage.setItem("history", cutPreHistory + "," + JSON.stringify(historyElement) + "]");
+    }
+
+    var searchCounter = localStorage.getItem("searchCounter");
+
+    if(searchCounter == "") {
+      localStorage.setItem("searchCounter", 1);
+    }
+    else {
+      localStorage.setItem("searchCounter", parseInt(searchCounter)+1)
+    }
+  }
+
+  console.log(JSON.parse(localStorage.getItem("history")).reverse()[0]);
+}
+
+
+
+
+
 class Search {
   constructor(name = "", source = "") {
     this.name = name;
@@ -78,6 +156,7 @@ class Search {
     Search.setSearch(document.getElementById('searchSelector' + data).value);
     Search.setLink();
     Search.openLink();
+    setHistory("");
   }
 }
 
@@ -92,14 +171,12 @@ let GTEngToPl = new Search("Google Translator (ENG -> PL)", "https://translate.g
 document.querySelector("#searchText").addEventListener("keyup", function(event) {
   if (event.keyCode == 13) {
     Search.autoSearch();
-    setHistory();
   }
 });
 
 document.querySelector("#searchSelector").addEventListener("keyup", function(event) {
   if (event.keyCode == 13) {
     Search.autoSearch();
-    setHistory();
   }
 });
 
