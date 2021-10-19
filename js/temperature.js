053dc444ec10cc17f5a6f6c6bbe9c0b658db8824
jakubLabedowicz <jalabedowicz@gmail.com>
2021-10-19 15:26:58 +0200

    edit

--- js/temperature.js
+++ js/temperature.js
@@ -3,23 +3,29 @@ function temperature() {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
     var apiKey = "3162734543e0e661642a7ad366e4f669";
+    var temperature1 = 0;
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + apiKey + '&lang=pl')
     .then(response => response.json())
     .then(data => data.main)
     .then(data => data.temp)
-    .then(data => Math.round(data))
-    .then(data => document.getElementById('temperature').innerHTML = data + '°C');
+    .then(function(data) {
+      document.getElementById('temperature').innerHTML = Math.round(data) + '°C'
+      console.log('Pobrano temperaturę: ' + data + '°C')
+    });
   }
 
   function error() {
     console.log('Unable to retrieve your location');
     var apiKey = "3162734543e0e661642a7ad366e4f669";
+    var temperature1 = 0;
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=' + apiKey + '&lang=pl')
     .then(response => response.json())
     .then(data => data.main)
     .then(data => data.temp)
-    .then(data => Math.round(data))
-    .then(data => document.getElementById('temperature').innerHTML = data + '°C');
+    .then(function(data) {
+      document.getElementById('temperature').innerHTML = Math.round(data) + '°C'
+      console.log('Pobrano temperaturę: ' + data + '°C')
+    });
   }
 
   if(!navigator.geolocation) {
