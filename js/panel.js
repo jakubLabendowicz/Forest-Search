ef80eb63e76205d0b632cb68f39de53cf14ce726
jakubLabedowicz <jalabedowicz@gmail.com>
2021-10-19 16:12:02 +0200

    edit

--- js/panel.js
+++ js/panel.js
@@ -35,5 +35,5 @@ panelController.addState(closePanel);
 panelController.addState(smallPanel);
 panelController.addState(mediumPanel);
 panelController.addState(largePanel);
-panelController.setFirstState(2);
+panelController.setFirstState(3);
 panelController.run();
